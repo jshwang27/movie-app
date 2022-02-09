659f2728fb54fd404d6d422cd86462b9872c481f
jshwang27 <jshwang27@gmail.com>
2022-02-10 02:04:40 +0900

    #1.1 movie-app

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         강의에서 사용된 부분으로 6버전 이전에 사용한 switch 를 사용한 방식 - npm i react-router-dom@5.3.0 5버전을 쓰면 해결 */}
         <Route path="/movie/:id" element={<Detail />} />
         {/* :이름 넘겨주는 값(변수)을 나타냄. 반드시 : 을 붙일 것. */}
-        <Route path="/" element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
       </Routes>
     </Router>
   );
