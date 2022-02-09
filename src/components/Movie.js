4a4dc3d8a901572743b6cb73d4bd8bfa3cff1d5d
jshwang27 <jshwang27@gmail.com>
2022-02-10 01:39:17 +0900

    #1.0 movie-app

---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ id, medium_cover_image, title, summary, genres }) {
+  return (
+    <div>
+      <img src={medium_cover_image} alt="img" />
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+        {/*react에서 페이지를 이동하는 방법. Link를 사용. */}
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  medium_cover_image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
---
+++ src/routes/Detail.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+
+    console.log(json.data.movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt="img" />
+          <h2>{movie.title}</h2>
+          <p>{movie.year}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+          <p>{movie.description_full}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
